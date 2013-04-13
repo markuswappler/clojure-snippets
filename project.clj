53a4d95641330009456bed7334331c0a6dd7b1d4
Markus Wappler <markus.wappler@mathematik.tu-chemnitz.de>
2013-04-13 16:55:47 +0200

    explore

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/markuswappler/clojure-snippets"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
                  [org.clojure/math.combinatorics "0.0.3"]
                  [the/parsatron "0.0.4"]
                  [org.clojure/math.numeric-tower "0.0.2"]])
--- src/clojure_snippets/explore.clj
+++ src/clojure_snippets/explore.clj
@@ -8,3 +8,20 @@
 (def unchunks
   (map #(count (take-while (partial >= %) unchunks)) 
        (reductions (fn [_ x] x) (range))))
+
+(defn thread-cond [ops x]
+  (cond-> x
+          (some #{:inc} ops) (inc)
+          (some #{:dbl} ops) (* 2)))
+
+(defn thread-as [x]
+  (as-> x name
+        (+ name 3)
+        (+ 4 name)))
+
+(defn thread-some [x]
+  (let [half (fn [x] (when (even? x) (/ x 2)))]
+    (some-> x
+            (half)
+            (half)
+            (half))))
